3b8a247967156afe68351ef08d810e2af4f1fd76
Kalob Porter <kalob@kalob.net>
2018-01-17 10:05:42 -0500

    Update addNote.jsx

--- components/addNote.jsx
+++ components/addNote.jsx
@@ -1,44 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Main from '../main.js';
 import Index from './index.jsx';
 
-class addNote extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      title: '',
-      text:  '',
-    };
-
-    this.handleTitleChange = this.handleTitleChange.bind(this);
-    this.handleTextChange = this.handleTextChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+export default class addNote extends React.Component {
+  state = {
+    title: '',
+    text: '',
   }
 
-  handleTitleChange(event){
-    this.setState({title: event.target.value});
+  handleChange = e => {
+    // Use the element name in order to only create one change function
+    this.setState({ [e.target.name]: e.target.value });
   }
-  handleTextChange(event){
-    this.setState({text: event.target.value});
-  }
+
   handleSubmit(event) {
     this.props.pushNote([this.state.title, this.state.text]);
   }
 
   render(){
-    var style = {
+    // Always use const or let, never var
+    const style = {
       'width': '100%',
       'textAlign': 'center',
       'backgroundColor': 'rgb(0, 200, 200)',
       'paddingBottom': '5px',
     }
     return (
-      <div style = {style}>
-            <input type="text" ref="title" value={this.state.title} onChange={this.handleTitleChange} placeholder='Title'/>
-            <input type="text" ref="text"  value={this.state.text} onChange={this.handleTextChange} placeholder='Text'/>
+      <div style={style}>
+            <input type="text" name="title" value={this.state.title} onChange={this.handleChange} placeholder='Title'/>
+            <input type="text" name="text"  value={this.state.text} onChange={this.handleChange} placeholder='Text'/>
             <button type="submit" value="Submit" onClick={this.handleSubmit}>Submit</button>
       </div>
     );
   }
 }
-export default addNote;
