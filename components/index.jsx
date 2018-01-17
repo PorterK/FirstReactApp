06b703518bb4f8193422913b0bf78015b8586b4d
Kalob Porter <kalob@kalob.net>
2018-01-17 10:01:52 -0500

    Update index.jsx

--- components/index.jsx
+++ components/index.jsx
@@ -1,32 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import AddNote from './addNote.jsx';
 import ListNotes from './notes/listNotes.jsx';
 
-class Index extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      notes: [
-        ["First Note", "First Note Test Text"],
-        ["Second Note", "Second Note Test Text"],
-      ],
-    }
+export default class Index extends Component {
+  // Removed constructor here because all we were doing was setting up the default state
+  state = {
+    notes: [
+      ["First Note", "First Note Test Text"],
+      ["Second Note", "Second Note Test Text"],
+    ],
   }
+
+  // Using an arrow function binds `this` to the function so we don't have to.
+  pushNote = data => {
+    const { notes } = this.state;
+
+    // Here we clone the notes array in order to keep the component state immutable.
+    const newNotesArr = notes.slice(0);
+
+    // We want to push the data to the new array that we created instead of to `this.state.notes`
+    newNotesArr.push(data);
+
+    // Finally set the state
+    this.setState({ notes: newNotesArr });
+  }
+
   render(){
-    this.pushNote = function(newNote){
-      var notes = this.state.notes;
-      notes.push(newNote);
-      this.setState({notes: notes})
-      console.log(this.state.notes);
-    }
     return(
       <div>
-        <AddNote pushNote={this.pushNote.bind(this)}/>
+        <AddNote pushNote={this.pushNote}/>
         <ListNotes notes={this.state.notes} />
       </div>
     );
   }
 }
-export default Index;
